0463ab9284d222c2ea3f12b7f1cbc52924950f3c
Taha27102710 <151642427+Taha27102710@users.noreply.github.com>
2023-12-28 10:21:26 -0800

    Add files via upload

---
+++ ChillThrills/script.js
@@ -0,0 +1,20 @@
+let Basic = document.getElementById("Basic");
+let Standard = document.getElementById("Standard");
+let Premium = document.getElementById("Premium");
+
+
+
+
+Basic.addEventListener('click', () => {
+    alert("You have purchased the basic plan");
+});
+
+Standard.addEventListener('click', () => {
+    alert("You have purchased the Standard plan");
+});
+
+
+
+Premium.addEventListener('click', () => {
+    alert("You have purchased the Premium plan");
+});
---
+++ ChillThrills/script2.js
@@ -0,0 +1,21 @@
+let chocolate = document.getElementById("chocolate");
+let strawberry = document.getElementById("strawberry");
+let pista = document.getElementById("pista");
+let oreo = document.getElementById("oreo");
+
+
+chocolate.addEventListener('click', () => {
+    alert("You have purchased the Chocolate Icecream");
+});
+
+strawberry.addEventListener('click', () => {
+    alert("You have purchased the Strawberry Icecream");
+});
+
+pista.addEventListener('click', () => {
+    alert("You have purchased the Pista Icecream");
+});
+
+oreo.addEventListener('click', () => {
+    alert("You have purchased the Oreo Icecream");
+});
